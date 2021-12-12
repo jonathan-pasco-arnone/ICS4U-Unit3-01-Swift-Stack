258f88daa90a540b0a06e76b871940a38687ed8f
Ubuntu <ubuntu@ip-172-31-84-55.ec2.internal>
2021-12-12 02:31:48 +0000

    corrected array order

--- MrCoxallStack.swift
+++ MrCoxallStack.swift
@@ -1,9 +1,12 @@
 class MrCoxallStack {
   var stackAsArray: [Int] = []
+
   func push(pushNumber: Int) {
-    stackAsArray.append(pushNumber)
+    stackAsArray.insert(pushNumber, at: 0)
   }
+
   func showStack() {
     print(stackAsArray)
   }
+
 }
